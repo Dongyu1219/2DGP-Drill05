764abb832c9ded10efe06cba131cf539723780cc
donggyukim@gram <1219kdk@gmail.com>
2024-09-30 17:50:33 +0900

    1차 완료

--- move_my_character_with_key_2023180009.py
+++ move_my_character_with_key_2023180009.py
@@ -8,9 +8,9 @@ character = load_image('drill_05_png.png')
 
 
 def handle_events():
-    global running ,dir
+    global running ,dir, dir2
     # fill here
-    global x
+    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -21,6 +21,10 @@ def handle_events():
                 dir += 1
             elif event.key == SDLK_LEFT:
                 dir -= 1
+            elif event.key == SDLK_UP:
+                dir2 += 1
+            elif event.key == SDLK_DOWN:
+                dir2 -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -28,21 +32,27 @@ def handle_events():
                 dir -= 1
             elif event.key == SDLK_LEFT:
                 dir += 1
-
+            elif event.key == SDLK_UP:
+                dir2 -= 1
+            elif event.key == SDLK_DOWN:
+                dir2 += 1
 running = True
 x = 800 // 2
+y= 600 //2
 frame = 0
 dir = 0         # 방향 변수 추가. 오른쪽 키 쭈욱 누르면 계속 가게
+dir2 = 0
 # fill here
 while running:
     clear_canvas()
     grass.draw(800 // 2, 600 // 2, 800, 600)
-    character.clip_draw(frame*130, 140, 130,140, x, 90)
+    character.clip_draw(frame*130, 140, 130,140, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) %5
     x += dir * 5                #방향 변수에 따라 x가 계속 변한다
-    delay(0.1)
+    y += dir2 * 5
+    delay(0.08)
 
 close_canvas()
 
